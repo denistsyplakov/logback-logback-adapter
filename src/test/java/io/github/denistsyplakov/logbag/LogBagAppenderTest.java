3257053e04d9e74c8e090a8b472f83a369fe56a5
denis.tsyplakov <denis.tsyplakov@gmail.com>
2024-03-26 17:58:23 +0400

    basic test

--- src/test/java/io/github/denistsyplakov/logbag/LogBagAppenderTest.java
+++ src/test/java/io/github/denistsyplakov/logbag/LogBagAppenderTest.java
@@ -6,25 +6,25 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Unit test for simple App.
+ * Unit test for logbag appender.
  */
-public class LogBagAppenderTest
-{
+public class LogBagAppenderTest {
 
     Logger log;
 
     @BeforeEach
     public void setup() {
+        TestAppender.clean();
         log = LoggerFactory.getLogger(LogBagAppenderTest.class);
     }
 
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void testConfigurationWorks() {
         log.info("Test message");
-        assertTrue( true );
+        assertEquals(1, TestAppender.logs.size());
+        assertEquals("Test message", TestAppender.logs.getFirst().getFormattedMessage());
     }
 }
